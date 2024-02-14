658def424994ab6634c45821ca9f4ba76ca3ed24
nikhilsinha822 <nikhilsinha822@gmail.com>
2024-02-14 21:59:17 +0530

    category schema created

---
+++ models/category.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    tags:[String]
+}, {timestamps : true})
+
+module.exports = mongoose.model('Category', categorySchema)
+
